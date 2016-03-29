cb7ec34124a25ee379309ae2cfa90604496d5fc4
Garrett Wilson <g@floft.net>
2016-03-29 10:26:04 -0700

    Use Boost everywhere instead of C++11
    Again, C++11 wasn't implemented in the VS2008 compiler

--- simulation/cpp-to-python/server.cpp
+++ simulation/cpp-to-python/server.cpp
@@ -15,21 +15,29 @@
 // http://think-async.com/Asio/asio-1.10.6/doc/asio/tutorial/tutdaytime3/src.html
 // http://think-async.com/Asio/asio-1.10.6/src/examples/cpp11/chat/chat_server.cpp
 //
+// For non-C++11, using Boost instead:
+// http://think-async.com/Asio/boost_asio_1_10_6/doc/html/boost_asio/example/cpp03/chat/chat_server.cpp
+//
 #include <set>
-#include <atomic>
 #include <ctime>
-#include <mutex>
-#include <thread>
 #include <deque>
 #include <string>
-#include <memory>
+#include <cstdlib>
 #include <iostream>
-#include <functional>
-#include <asio.hpp>
+#include <algorithm>
+#include <boost/asio.hpp>
+#include <boost/bind.hpp>
+#include <boost/chrono.hpp>
+#include <boost/atomic.hpp>
+#include <boost/thread.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/make_shared.hpp>
+#include <boost/enable_shared_from_this.hpp>
 #include <rapidjson/document.h>
 #include <rapidjson/writer.h>
 #include <rapidjson/stringbuffer.h>
 
+namespace asio = boost::asio;
 using asio::ip::tcp;
 
 // Get current date string, for debugging
@@ -190,16 +198,16 @@ public:
 // Keep a list of all connections and allow pushing data out to all of
 // them
 //
-typedef std::shared_ptr<Connection> connection_ptr;
+typedef boost::shared_ptr<Connection> connection_ptr;
 
 class ConnectionManager
 {
     enum { max_recent_data = 100 };
     enum { max_commands = 100 };
     std::set<connection_ptr> connections;
-    std::mutex data_mutex;
+    boost::mutex data_mutex;
     std::deque<AutopilotData> recent_data;
-    std::mutex commands_mutex;
+    boost::mutex commands_mutex;
     std::deque<AutopilotCommand> commands;
 
 public:
@@ -212,10 +220,10 @@ public:
     {
         connections.insert(con);
 
-        std::lock_guard<std::mutex> guard(data_mutex);
+        boost::lock_guard<boost::mutex> guard(data_mutex);
 
-        for (auto d : recent_data)
-            con->push(d);
+        std::for_each(recent_data.begin(), recent_data.end(),
+                boost::bind(&Connection::push, con, _1));
     }
 
     void removeConnection(connection_ptr con)
@@ -226,7 +234,7 @@ public:
     // Send new data to all connections
     void push(const AutopilotData& d)
     {
-        std::lock_guard<std::mutex> guard(data_mutex);
+        boost::lock_guard<boost::mutex> guard(data_mutex);
 
         // Save new data
         recent_data.push_back(d);
@@ -236,14 +244,14 @@ public:
             recent_data.pop_front();
 
         // Push this data out to all the connections
-        for (auto con : connections)
-            con->push(d);
+        std::for_each(connections.begin(), connections.end(),
+                boost::bind(&Connection::push, _1, boost::ref(d)));
     }
 
     // When a connection sends a command, save it
     void processCommand(const AutopilotCommand& c)
     {
-        std::lock_guard<std::mutex> guard(commands_mutex);
+        boost::lock_guard<boost::mutex> guard(commands_mutex);
         commands.push_back(c);
 
         while (commands.size() > max_commands)
@@ -255,14 +263,14 @@ public:
     // If using polling, see if there's any new commands available
     bool pullCommandAvailable()
     {
-        std::lock_guard<std::mutex> guard(commands_mutex);
+        boost::lock_guard<boost::mutex> guard(commands_mutex);
         return !commands.empty();
     }
 
     // Get the oldest command that we haven't already processed
     AutopilotCommand pullCommand()
     {
-        std::lock_guard<std::mutex> guard(commands_mutex);
+        boost::lock_guard<boost::mutex> guard(commands_mutex);
         AutopilotCommand c = commands.front();
         commands.pop_front();
         return c;
@@ -275,7 +283,7 @@ public:
 //
 class Session
     : public Connection,
-      public std::enable_shared_from_this<Session>
+      public boost::enable_shared_from_this<Session>
 {
     tcp::socket socket;
     ConnectionManager& manager;
@@ -283,9 +291,14 @@ class Session
     std::deque<AutopilotData> write_data;
 
 public:
-    Session(tcp::socket socket, ConnectionManager& manager)
-        : socket(std::move(socket)), manager(manager)
+    Session(asio::io_service& io_service, ConnectionManager& manager)
+        : socket(io_service), manager(manager)
     {
+    }
+
+    tcp::socket& getSocket()
+    {
+        return socket;
     }
 
     // Start this session
@@ -317,105 +330,112 @@ private:
     // Read commands from the clients
     void do_read()
     {
-        auto self(shared_from_this());
-
         // Read from connection until an \0
         //
         // See: http://think-async.com/Asio/asio-1.10.6/doc/asio/overview/core/line_based.html
         asio::async_read_until(socket, streambuf, '\0',
-                [this, self](std::error_code ec, std::size_t /*length*/)
-                {
-                    if (!ec)
-                    {
-                        // Input stream from this ASIO stream buf
-                        std::istream is(&streambuf);
+                boost::bind(&Session::handle_read, shared_from_this(),
+                    boost::asio::placeholders::error));
+    }
 
-                        // Get up to the \0
-                        std::string command;
-                        std::getline(is, command, '\0');
+    void handle_read(const boost::system::error_code& ec)
+    {
+        if (!ec)
+        {
+            // Input stream from this ASIO stream buf
+            std::istream is(&streambuf);
 
-                        // Do something with the command we read
-                        manager.processCommand(command);
+            // Get up to the \0
+            std::string command;
+            std::getline(is, command, '\0');
 
-                        // Wait for another message
-                        do_read();
-                    }
-                    else
-                    {
-                        std::cerr << "Error: reading, closing connection" << std::endl;
+            // Do something with the command we read
+            manager.processCommand(command);
 
-                        // An error occured, so remove this current
-                        // connection
-                        manager.removeConnection(shared_from_this());
-                    }
-                }
-        );
+            // Wait for another message
+            do_read();
+        }
+        else
+        {
+            std::cerr << "Note: could not read, closing connection" << std::endl;
+
+            // An error occured, so remove this current
+            // connection
+            manager.removeConnection(shared_from_this());
+        }
     }
 
     // Write autopilot data to the connection
     void do_write()
     {
-        auto self(shared_from_this());
         std::string data = write_data.front().str() + '\0';
         asio::async_write(socket,
                 asio::buffer(data, data.length()),
-                [this, self](std::error_code ec, std::size_t /*length*/)
-                {
-                    if (!ec)
-                    {
-                        // Remove from the queue since now we've written
-                        // this data
-                        write_data.pop_front();
+                boost::bind(&Session::handle_write, shared_from_this(),
+                    boost::asio::placeholders::error));
+    }
 
-                        // If the queue isn't empty, then write the next
-                        // data as well
-                        if (!write_data.empty())
-                            do_write();
-                    }
-                    else
-                    {
-                        std::cerr << "Error: writing, closing connection" << std::endl;
+    void handle_write(const boost::system::error_code& ec)
+    {
+        if (!ec)
+        {
+            // Remove from the queue since now we've written
+            // this data
+            write_data.pop_front();
 
-                        // An error occured, so remove this current
-                        // connection
-                        manager.removeConnection(shared_from_this());
-                    }
-                }
-        );
+            // If the queue isn't empty, then write the next
+            // data as well
+            if (!write_data.empty())
+                do_write();
+        }
+        else
+        {
+            std::cerr << "Note: could not write, closing connection" << std::endl;
+
+            // An error occured, so remove this current
+            // connection
+            manager.removeConnection(shared_from_this());
+        }
     }
 };
+
+typedef boost::shared_ptr<Session> session_ptr;
 
 //
 // Listen on a port for incomming client connections
 //
 class Server
 {
+    asio::io_service& io_service;
     tcp::acceptor acceptor;
-    tcp::socket socket;
     ConnectionManager& manager;
 
 public:
     Server(asio::io_service& io_service, int port, ConnectionManager&
             manager)
-        : acceptor(io_service, tcp::endpoint(tcp::v4(), port)),
-          socket(io_service),
+        : io_service(io_service),
+          acceptor(io_service, tcp::endpoint(tcp::v4(), port)),
           manager(manager)
     {
-        do_accept();
+        start_accept();
     }
 
 private:
-    void do_accept()
+    void start_accept()
     {
-        acceptor.async_accept(socket,
-                [this](std::error_code ec)
-                {
-                    if (!ec)
-                        std::make_shared<Session>(std::move(socket),
-                                manager)->start();
+        session_ptr new_session(new Session(io_service, manager));
+        acceptor.async_accept(new_session->getSocket(),
+                boost::bind(&Server::handle_accept, this, new_session,
+                    boost::asio::placeholders::error));
+    }
 
-                    do_accept();
-                });
+    void handle_accept(session_ptr session,
+            const boost::system::error_code& ec)
+    {
+        if (!ec)
+            session->start();
+
+        start_accept();
     }
 };
 
@@ -426,9 +446,9 @@ private:
 //
 class AutopilotThread
 {
-    std::atomic_bool exit;
+    boost::atomic<bool> exit;
     ConnectionManager& manager;
-    std::thread t;
+    boost::thread t;
 
 public:
     AutopilotThread(ConnectionManager& manager)
@@ -465,7 +485,7 @@ public:
             }
 
             // Wait a little bit before sending more data
-            std::this_thread::sleep_for(std::chrono::seconds(5));
+            boost::this_thread::sleep_for(boost::chrono::seconds(5));
         }
     }
 
@@ -493,7 +513,7 @@ int main(int argc, char* argv[])
         }
 
         // Port we want to run the server on
-        int port = std::stoi(argv[1]);
+        int port = std::atoi(argv[1]);
 
         // Manage data between autopilot and network clients
         ConnectionManager manager;
